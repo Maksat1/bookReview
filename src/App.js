13b1556e417ed14077dab506605b333cb27a25cd
Maksat1 <Maksat.bimakov@gmail.com>
2023-05-24 22:46:57 +0600

    add react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+const API_ENDPOINT = 'http://localhost:5000/books'
 
 function App() {
+  const [books, setBooks] = useState([]);
+  
+  useEffect(() => {
+    console.log('use effect hook triggered')
+
+    fetch(API_ENDPOINT)
+      .then(response => response.json())
+      .then(data => {
+        console.log('API endpoint called successfully')
+        setBooks(data)
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+  console.log('Component rendered')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Book List</h1>
+      <ul>
+        {books.map(book => (
+          <li key={book._id}>
+            <h3>{book.title}</h3>
+            <p>{book.author}</p>
+            <ul>
+              {Object.entries(book.reviews).map(([key, value]) => (
+                <li key={key}>
+                  <strong>Review {key}:</strong> {value}
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default App
